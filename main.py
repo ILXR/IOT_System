b94fd79a67e1d70ebe1260bf6fd3aab7d56a90e3
GYC <gyc8787@163.com>
2021-12-28 21:47:59 +0800

    修改了socket通信协议

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def get_switch_status():
 
 def database_init():
     global DATABASE, DATABASE_NAME
-    print("database init...")
+    #print("database init...")
     try:
         DATABASE_FILE = open(DATABASE_NAME, 'r+b')
     except OSError:
@@ -54,28 +54,28 @@ def database_init():
     if(SWITCH_KEY not in DATABASE):
         DATABASE[SWITCH_KEY] = SWITCH_ON
         DATABASE.flush()
-    print("database connected\n")
+    #print("database connected\n")
 
 
 def switch_gpio_init():
     # 初始化GPIO
     global GPIO_PIN, GPIO_PIN_NUMBER, LED_PIN, LED_PIN_NUMBER
-    print("switch init...")
+    #print("switch init...")
     GPIO_PIN = Pin(GPIO_PIN_NUMBER, Pin.OUT, value=0)
     LED_PIN = Pin(LED_PIN_NUMBER, Pin.OUT, value=1)
-    print("switch default on\n")
+    #print("switch default on\n")
 
 
 def switch_init_default():
     global GPIO_PIN, GPIO_PIN_NUMBER, LED_PIN, LED_PIN_NUMBER
-    print("set switch status default on")
+    #print("set switch status default on")
     GPIO_PIN.off()
     LED_PIN.on()
 
 
 def switch_init_memory():
     global DATABASE, GPIO_PIN, LED_PIN
-    print("set switch status according to the last setting")
+    #print("set switch status according to the last setting")
     if get_switch_status() == SWITCH_ON:
         switch_open()
     else:
@@ -87,7 +87,7 @@ def switch_open():
     GPIO_PIN.off()
     LED_PIN.on()
     DATABASE[SWITCH_KEY] = SWITCH_ON
-    print("switch open")
+    #print("switch open")
     DATABASE.flush()
 
 
@@ -96,7 +96,7 @@ def switch_close():
     GPIO_PIN.on()
     LED_PIN.off()
     DATABASE[SWITCH_KEY] = SWITCH_OFF
-    print("switch close")
+    #print("switch close")
     DATABASE.flush()
 
 
@@ -117,7 +117,8 @@ def json2bytes(obj):
         res = str.encode(
             json.dumps(obj), 'GBK')
     except Exception as e:
-        print(e, "\njson to bytes failed:", obj)
+        #print(e, "\njson to bytes failed:", obj)
+        pass
     finally:
         return res
 
@@ -128,7 +129,8 @@ def bytes2json(obj):
         res = json.loads(bytes.decode(
             obj, 'GBK'))
     except Exception as e:
-        print(e, "\nbytes to json failed:", obj)
+        #print(e, "\nbytes to json failed:", obj)
+        pass
     finally:
         return res
 
@@ -138,28 +140,27 @@ def wifi_connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
+        #print('connecting to network...')
         wlan.connect(SSID, PASSWD)
         while not wlan.isconnected():
             try:
                 time.sleep(0.1)
             except KeyboardInterrupt:
                 exit()
-    print('wifi connected to ', SSID)
-    print('network config: [local ip: {}\tgateway: {}\tdns: {}]'.format(
-        wlan.ifconfig()[0], wlan.ifconfig()[2], wlan.ifconfig()[3]))
+    #print('wifi connected to ', SSID)
+    #print('network config: [local ip: {}\tgateway: {}\tdns: {}]'.format(wlan.ifconfig()[0], wlan.ifconfig()[2], wlan.ifconfig()[3]))
     MAC_ADDRESS = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
-    print("mac address: ", MAC_ADDRESS, "\n")
+    #print("mac address: ", MAC_ADDRESS, "\n")
 
 
 def socket_connect():
     global MAC_ADDRESS
     s = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
     info = usocket.getaddrinfo(HOST, PORT, 0, usocket.SOCK_STREAM)
-    print("socket info: ", info, info[0][-1])
-    print("socket connecting...")
+    #print("socket info: ", info, info[0][-1])
+    #print("socket connecting...")
     s.connect(info[0][-1])
-    print("socket connected\n")
+    #print("socket connected\n")
     # send device info
     device_info = {
         'mac': MAC_ADDRESS,
@@ -169,16 +170,20 @@ def socket_connect():
     # 一切初始化成功，这时再根据数据库来修改开关状态
     switch_init_memory()
     # recv action data
-    print("\nlistening to socket...")
+    #print("\nlistening to socket...")
     while True:
         try:
-            data = s.recv(1024)
+            length = s.recv(1)[0]
+            #print("recv data , length : ", length)
+            if(length == 0):
+                break
+            data = s.recv(length)
             if data:
                 data = bytes2json(data)
                 if data is not None:
-                    print("get data: ", data)
+                    #print("get data: ", data)
                     if 'action' in data.keys():
-                        print("get action, my id: ", data['action'])
+                        #print("get action, my id: ", data['action'])
                         switch_ctrl()
             else:
                 time.sleep(0.01)
@@ -197,14 +202,14 @@ def start_serve():
         except KeyboardInterrupt as e:
             exit()
         except Exception as e:
-            print(e)
+            # print(e)
             time.sleep(1)
-            print("---------- retry main -----------")
+            #print("---------- retry main -----------")
             continue
 
 
 def main():
-    print("---------- start main -----------")
+    #print("---------- start main -----------")
     database_init()
     switch_gpio_init()
     start_serve()
